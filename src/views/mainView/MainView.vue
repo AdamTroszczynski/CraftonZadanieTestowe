7d6552a067c3e7098029fd8dbf1d2057272621b2
Amanowsky <adamtroszczynski@outlook.com>
2025-08-28 02:18:41 +0200

    feat: connect sections to mainView

---
+++ src/views/mainView/MainView.vue
@@ -0,0 +1,13 @@
+<template>
+  <TheNavbar />
+  <HeroSection />
+  <TextInvestmentsSection />
+  <SliderSection />
+</template>
+
+<script setup lang="ts">
+import TheNavbar from '@/widgets/TheNavbar.vue';
+import HeroSection from '@/views/mainView/sections/HeroSection.vue';
+import TextInvestmentsSection from '@/views/mainView/sections/TextInvestmentsSection.vue';
+import SliderSection from '@/views/mainView/sections/SliderSection.vue';
+</script>
