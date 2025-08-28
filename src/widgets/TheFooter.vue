cd58d77ddadeb80009b0eb717fe5dfafcb7fea54
Amanowsky <adamtroszczynski@outlook.com>
2025-08-28 15:01:47 +0200

    feat: add footer

---
+++ src/widgets/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="3xl:px-18 flex flex-col items-center gap-8 px-11 pt-10 pb-15 2xl:px-16">
+    <p class="d-body-regular-m text-text text-center">
+      © 2025 RealEstate Sp. z o.o. Wszelkie prawa zastrzeżone.
+    </p>
+    <p class="d-body-regular-m text-text">
+      Projekt i realizacja: <span class="font-semibold">Crafton</span>
+    </p>
+    <p class="text-text d-body-regular-s max-w-[694px] text-center">
+      Materiały zawarte na stronie WWW mają charakter poglądowy i nie mogą być traktowane jako
+      ostateczne projekty realizacyjne. Deweloper zastrzega sobie prawo zmian. Niniejsza informacja
+      nie stanowi oferty w rozumieniu przepisów Kodeksu Cywilnego i ma wyłącznie charakter
+      informacyjny.
+    </p>
+  </footer>
+</template>
