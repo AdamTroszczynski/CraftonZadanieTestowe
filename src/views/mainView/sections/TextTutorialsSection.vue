0aa5c82c24fcb3714a28bca0c7d2454f51dc4faf
Amanowsky <adamtroszczynski@outlook.com>
2025-08-28 15:04:42 +0200

    feat: add textTutorial section

---
+++ src/views/mainView/sections/TextTutorialsSection.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="3xl:p-18 p-11 2xl:p-16">
+    <h2
+      class="text-dark d-headline-h2 sm:d-headline-h1 text-center uppercase lg:max-w-[518px] lg:text-start"
+    >
+      Poradnik <br />
+      po nieruchomościach
+    </h2>
+  </section>
+</template>
