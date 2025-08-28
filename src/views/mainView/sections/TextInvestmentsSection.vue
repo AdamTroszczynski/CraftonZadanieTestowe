175feccbb6cd31862d7c9a5352790bdf4f05e731
Amanowsky <adamtroszczynski@outlook.com>
2025-08-28 02:18:05 +0200

    feat: add textInvestmentsSection

---
+++ src/views/mainView/sections/TextInvestmentsSection.vue
@@ -0,0 +1,12 @@
+<template>
+  <section
+    class="3xl:px-17 3xl:pb-17 flex flex-col items-center justify-center gap-9 px-10 pb-10 2xl:px-15 2xl:pb-15"
+  >
+    <h1 class="d-headline-h1 text-dark text-center uppercase">Nasze inwestycje</h1>
+    <p class="text-text d-body-regular-l max-w-[750px] text-center">
+      Nasze inwestycje to miejsca, które łączą nowoczesny design, funkcjonalność i trwałość. Każdy
+      projekt realizowany przez RealEstate to wynik pasji, zaangażowania i dbałości o każdy
+      szczegół.
+    </p>
+  </section>
+</template>
