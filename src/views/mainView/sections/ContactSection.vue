124ef8e9d773579adf9babd2be9682803a8230db
Amanowsky <adamtroszczynski@outlook.com>
2025-08-28 15:05:14 +0200

    feat: add contact section

---
+++ src/views/mainView/sections/ContactSection.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="3xl:p-18 bg-surface-subtle flex flex-col gap-10 px-11 py-16 2xl:px-16">
+    <h2 class="text-dark d-headline-h2 lg:d-headline-h1 uppercase">Porozmawiajmy</h2>
+    <ContactForm />
+  </section>
+</template>
+
+<script setup lang="ts">
+import ContactForm from '@/views/mainView/widgets/ContactForm.vue';
+</script>
