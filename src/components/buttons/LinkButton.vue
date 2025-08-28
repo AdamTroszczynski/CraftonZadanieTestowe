f0e3048cc10484343c4c7f62a16751a9541669db
Amanowsky <adamtroszczynski@outlook.com>
2025-08-28 02:17:19 +0200

    feat: add LinkButton component

---
+++ src/components/buttons/LinkButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <a
+    class="xl:d-headline-h5 d-headline-h4 text-dark hover:text-text active:stroke-on-dark flex cursor-pointer items-center uppercase"
+    :class="isActive ? 'text-primary' : 'text-dark'"
+  >
+    <slot />
+    <BaseIcon v-if="withArrow" name="navArrow" />
+  </a>
+</template>
+
+<script setup lang="ts">
+import BaseIcon from '@/components/icons/BaseIcon.vue';
+
+defineProps({
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  withArrow: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
