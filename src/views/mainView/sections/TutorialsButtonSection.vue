c6b487a2c447577fb5db631abb69347bb95cdbc1
Amanowsky <adamtroszczynski@outlook.com>
2025-08-28 15:04:59 +0200

    feat: add TutorialsButton section

---
+++ src/views/mainView/sections/TutorialsButtonSection.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="3xl:px-18 3xl:pb-18 flex justify-center px-11 pb-11 2xl:px-16 2xl:pb-16">
+    <BaseButton :is-blue="true" icon="arrow">Więcej poradników</BaseButton>
+  </section>
+</template>
+<script setup lang="ts">
+import BaseButton from '@/components/buttons/BaseButton.vue';
+</script>
